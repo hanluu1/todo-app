a7afcc055f611509a3ae61e41f82407cf18b4c6f
Han ne <Han@cs470.local>
2024-10-11 15:58:28 -0700

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, {useState} from "react";
 import Form from "@/components/Form";
 import Header from "@/components/Header";
 import TODOHero from "@/components/TODOHero";
@@ -7,25 +7,20 @@ import TODOList from "@/components/TODOList";
 import "./styles.css"
 
 function Home() {
-  const [todos, setTodos] = React.useState([
-    {
-      title: "Some task",  // string
-      id: self.crypto.randomUUID(), // string
-      is_completed: false // boolean
-      }, 
-      {
-        title: "Some other task",
-        id: self.crypto.randomUUID(),
-        is_completed: true,
-      },
-      { title: "last task", id: self.crypto.randomUUID(), is_completed: false },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const todos_completed = todos.filter(
+    (todos) => todos.is_completed === true
+  ).length;
+  const total_todos = todos.length;
   return (
     <div className="wrapper">
       <Header  />
-      <TODOHero  todos_completed={0} total_todos={0} />
-      <Form />
-      <TODOList todos={[todos]} setTodos={[setTodos]}/>
+      <TODOHero  todos_completed={todos_completed} total_todos={total_todos} />
+      <Form setTodos={setTodos}/>
+      
+      <TODOList todos={todos}/>
+        
+      
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,17 @@
 "use client";
 
-function Form() {
+function Form({setTodos}) {
     const handleSubmit = (event) => {
         event.preventDefault();
         // reset the form
+        const value = event.target.todo.value;
+        setTodos((prevTodos) => [
+            ...prevTodos,
+            { title: value, id: self.crypto.randomUUID(), is_completed: false },
+        ]);
         event.target.reset();
-      };
+  };
+      
       return (
         <form className="form" onSubmit={handleSubmit}>
           <label htmlFor="todo">
