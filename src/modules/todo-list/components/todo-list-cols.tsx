12cd2f3c79541a0d3eb89071e2a85f67310f1a1f
Long <time.turnback@gmail.com>
2024-11-14 15:36:54 +0700

    display col base on status exist

--- src/modules/todo-list/components/todo-list-cols.tsx
+++ src/modules/todo-list/components/todo-list-cols.tsx
@@ -2,13 +2,18 @@
 
 import React from 'react';
 
+import { useTodoStore } from '@/stores';
+
 import { TodoList } from './todo-list';
 
 export function TodoListCols() {
+  const todos = useTodoStore((state) => state.todos);
+  const showReady = todos.some((todo) => todo.status === 'Ready');
+
   return (
     <div className="grid grid-cols-4 gap-3">
       <TodoList status="" />
-      <TodoList status="Ready" />
+      {showReady && <TodoList status="Ready" />}
       <TodoList status="In progress" />
       <TodoList status="Completed" />
     </div>
